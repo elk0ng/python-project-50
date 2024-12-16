c0ca5daf5c6ae433dcb97d3f9d080f98f002fd74
elk0ng <elk0ng@icloud.com>
2024-12-16 16:39:14 +0300

    Added gendiff script file

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+import argparse
+from gendiff.diff import generate_diff
+
+
+string = 'Compares two configuration files and shows a difference.'
+parser = argparse.ArgumentParser(description=string)
+parser.add_argument('first_file', help='what about first file')
+parser.add_argument('second_file', help='what about second file')
+parser.add_argument(
+    "-f", "--format", default="stylish",
+    help="output format, default: 'stylish'"
+)
+args = parser.parse_args()
+
+
+def main():
+    print(generate_diff(args.first_file, args.second_file, args.format))
+
+
+if __name__ == '__main__':
+    main()
